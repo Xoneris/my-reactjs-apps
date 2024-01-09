fb1473bcc5fb9355646208a553d6005cc7d8d641
Xoneris <kely42@gmx.ch>
2024-01-09 01:44:47 +0100

    implemented the base function of most buttons. Still has a few errors that can happen

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -1,11 +1,85 @@
 import './style.css';
+import { useState } from 'react';
 
 function Calculator() {
 
+    let lastElementInDisplay;
+    let result = 0;
+
+    const [resultDisplay, setResultDisplay] = useState(0);
+    const [display, setDisplay] = useState(" ");
+    const [currentMathOperator, setCurrentMathOperator] = useState("");
+
+    const resetAll = () => {
+        setResultDisplay(0);
+        setDisplay("");
+    }
+
+    const resetDisplay = () => {
+        setDisplay(" ");
+    }
+
+    const addNumberToDisplay = (num) => {
+
+        lastElementInDisplay = display[display.length - 1]
+
+        if (lastElementInDisplay === "+" || lastElementInDisplay === "-" || lastElementInDisplay === "*" || lastElementInDisplay === "/"){
+            setDisplay(display + " " + num);
+            result = display + num;
+        }
+        else {
+            setDisplay(display + num);
+            result = display + num;
+        }
+        
+    }
+
+    const mathOperator = (operator) => {
+        
+        lastElementInDisplay = display[display.length - 1]
+        if (lastElementInDisplay !== "+" && lastElementInDisplay !== "-" && lastElementInDisplay !== "*" && lastElementInDisplay !== "/"){
+            setDisplay(display + " " + operator);
+        }
+        if (lastElementInDisplay !== operator){
+            
+        }
+        setCurrentMathOperator(operator);
+    }
+
+    const displayResult = () => {
+        setResultDisplay(eval(display));
+    }
+
     return (
-        <div className='calculator'>
+        <div>
             <h2>Calculator</h2>
+            <h3>{currentMathOperator}</h3>
+            <div className='calculator'> 
+                <div className='result'>{resultDisplay}</div>
+                <div className='display'>{display}</div>
+                <div><button onClick={resetAll}>CE</button></div>
+                <div><button onClick={resetDisplay}>C</button></div>
+                <div><button disabled>x^2</button></div>
+                <div><button onClick={(e) => mathOperator("/")}>/</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(7)}>7</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(8)}>8</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(9)}>9</button></div>
+                <div><button onClick={(e) => mathOperator("*")}>*</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(4)}>4</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(5)}>5</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(6)}>6</button></div>
+                <div><button onClick={(e) => mathOperator("-")}>-</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(1)}>1</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(2)}>2</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(3)}>3</button></div>
+                <div><button onClick={(e) => mathOperator("+")}>+</button></div>
+                <div><button disabled>+/-</button></div>
+                <div><button onClick={(e) => addNumberToDisplay(0)}>0</button></div>
+                <div><button disabled>.</button></div>
+                <div><button onClick={(e) => displayResult()}>=</button></div>
+            </div>
         </div>
+        
     )
 }
 
