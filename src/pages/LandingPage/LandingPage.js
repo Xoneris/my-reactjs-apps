afb8b3f733e2271d80c6becea59969637af7c34c
Xoneris <kely42@gmx.ch>
2024-01-23 02:41:51 +0100

    added some text and fixed some styling

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ function LandingPage() {
     return (
         <div>
             <h2>Main Area</h2>
+            <p>Hello!</p>
+            <p>This here is just a small collection of React Apps i've done for practice/learning.</p>
         </div>
     );
 }
